ef7029d7094dfd9b38e6f11bc421e8e5eb78e712
Trang Le <trangle1405@gmail.com>
2019-06-21 09:30:11 +0700

    error image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191); //NEW: Increase StringLength
+        Schema::defaultStringLength(191); //NEW: Increase StringLength+3
         URL::forceScheme('https');
     }
 }
